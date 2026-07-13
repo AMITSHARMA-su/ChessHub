edc78e713dee0a6011d2461d988db9b9dd16f8d7
AMITSHARMA-su <amitrsharma.200502@gmail.com>
2026-07-14 01:13:58 +0530

    Setup ASP.NET Identity and SQL Server database

--- ChessHub/Data/ApplicationDbContext.cs
+++ ChessHub/Data/ApplicationDbContext.cs
@@ -3,7 +3,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ChessHub.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
     }
 }
